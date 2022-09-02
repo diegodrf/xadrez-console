9c06f47f95eba2cee548936122b7fd2f45c5ef7e
Diego Faria <diego.rdfaria@gmail.com>
2022-09-02 00:54:35 -0300

    Corrigindo peça para iniciar com posição NULL

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -8,14 +8,13 @@ namespace xadrez_console.tabuleiro
 {
     class Peca
     {
-        public Posicao Posicao { get; set; }
+        public Posicao? Posicao { get; set; }
         public Cor Cor { get; set; }
         public int QuantidadeDeMovimentos { get; protected set; }
         public Tabuleiro Tabuleiro { get; protected set; }
 
-        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+        public Peca(Cor cor, Tabuleiro tabuleiro)
         {
-            Posicao = posicao;
             Cor = cor;
             QuantidadeDeMovimentos = 0;
             Tabuleiro = tabuleiro;
